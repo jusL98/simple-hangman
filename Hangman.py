b41ea43fb967413e27e47672b8637c189a2e8167
jusL98 <jus888lee@gmail.com>
2024-12-21 13:24:37 -0500

    first commit

---
+++ Hangman.py
@@ -0,0 +1,50 @@
+"""
+This is a simple hangman game where the user has to guess the food.
+"""
+
+# Imports
+import random
+import WordsForHangman
+
+# Variables
+words = WordsForHangman.words
+answer = random.choice(words)
+blanks = ['_'] * len(answer)
+attempts = 6
+guessed_letters = set()
+
+# Game
+print('****************************************')
+print(f'{"Welcome to the Hangman Game!" :^40}')
+print(f'{"All words are related to foods." :^40}')
+print(f'{"You have 6 incorrect guesses. Good luck." :^40}')
+print('****************************************')
+print()
+
+print(' '.join(blanks))
+
+while attempts > 0 and '_' in blanks: # Runs while the attempts are greater than 0 and there are still blanks.
+    guess = input('Enter your guess: ').lower()
+    
+    if guess in guessed_letters: # Checks if user has already guessed the letter.
+        print('You have already guessed that letter. Try again.\n')
+        continue
+    
+    guessed_letters.add(guess)
+    
+    if guess in answer: # Checks if the guess is in the answer.
+        for index, letter in enumerate(answer): # Iterates over each letter in the answer, collecting the index.
+            if letter == guess:
+                blanks[index] = guess # Replaces the blank with the guess.
+    else:
+        attempts -= 1
+        print(f'Wrong guess. You have {attempts} attempts left.')
+    
+    print()
+    print(' '.join(blanks))
+
+# End Of Game
+if '_' not in blanks:
+    print(f'Congratulations! You guessed the word: {answer}.')
+else:
+    print(f'Sorry, you ran out of attempts. The word was: {answer}.')
---
+++ README.md
@@ -0,0 +1,38 @@
+# Simple Hangman Game
+This is a simple hangman game related to foods to practice string manipulation. 
+
+## About This Project
+The game randomly selects a food-related word and the player has to guess the word by suggesting letters within a certain number of guesses.
+
+## Technologies Used
+Python 3.13
+
+## Installation
+To install the Simple Hangman Game, follow these steps:
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/yourusername/SimpleHangmanGame.git
+    cd SimpleHangmanGame
+    ```
+
+2. Ensure that you have python running on your system.
+
+3. Install the required dependencies:
+    ```bash
+    N/A
+    ```
+
+4. Run the Simple Food Hangman Game.
+    ```bash
+    # Example command
+    pip install project-name
+    ```
+
+## Usage
+- Enter a character to guess if it is in the word.
+- Repeat the process and continue guessing letters.
+- You have a max of 6 incorrect guesses. Good luck!
+
+---
+Thank you!
